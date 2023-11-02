44395b1c343c5ce562129be4ac8a6cb8f5c644f9
Kajetan Szal <kajsza@st.amu.edu.pl>
2023-11-02 19:21:47 +0100

    Add model creation test with iteration

--- library/tests/integration_tests/test_monitoring.py
+++ library/tests/integration_tests/test_monitoring.py
@@ -6,6 +6,7 @@ sys.path.append('..')
 import pytest
 
 import mlops.monitoring
+import mlops.tracking
 from mlops.config.config import settings as lib_settings
 from app.config.config import settings as app_settings
 from app.database.init_mongo_db import drop_database
@@ -31,7 +32,7 @@ async def test_create_model_successful(setup):
 
 
 @pytest.mark.asyncio
-async def test_create_project_failure(setup):
+async def test_create_model_failure(setup):
     await drop_database()
 
     model_name = 'test model'
@@ -60,5 +61,31 @@ async def test_set_active_model(setup):
     mlops.monitoring.set_active_model(model_name='test_model')
 
     assert lib_settings.active_model == 'test_model'
+
+
+@pytest.mark.asyncio
+async def test_create_model_with_iteration(setup):
+    await drop_database()
+
+    project = mlops.tracking.create_project(title='test_project')
+    experiment = mlops.tracking.create_experiment(name='test_experiment', project_id=project['_id'])
+
+    params = {'param1': 10, 'param2': 20, 'param3': 30}
+    metrics = {'metric1': 0.90, 'metric2': 1.3, 'metric3': 1000}
+
+    with mlops.tracking.start_iteration('test_iteration', project_id=project['_id'],
+                                        experiment_id=experiment['id']) as iteration:
+        iteration.log_model_name('test_iteration.py')
+        iteration.log_parameters(params)
+        iteration.log_metrics(metrics)
+
+    iteration_dict = iteration.end_iteration()
+
+    model_name = 'test_model'
+
+    response = mlops.monitoring.create_model(model_name=model_name, iteration_dict=iteration_dict)
+
+    assert response['model_name'] == model_name and response['model_status'] == 'active'
+
 
 
