75d27377fddb5f74022afb7ebafd1683b7c6a5c3
Paweł Łączkowski <broketmpl@gmail.com>
2023-11-04 22:39:01 +0100

    Add an initial version of the model view page

---
+++ front-new/src/components/modals/experiments/index.ts
@@ -0,0 +1,9 @@
+import CreateExperimentModal from "./create-experiment-modal";
+import EditExperimentModal from "./edit-experiment-modal";
+import DeleteExperimentModal from "./delete-experiment-modal";
+
+export {
+    CreateExperimentModal,
+    EditExperimentModal,
+    DeleteExperimentModal,
+};
