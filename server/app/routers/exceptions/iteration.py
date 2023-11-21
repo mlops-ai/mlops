53ff01e2ad844c2307c11bf30bd37b199f2f8382
Kacper Pękalski <kac.pekalski1@gmail.com>
2023-11-21 19:48:08 +0100

    Fixed deleting Project and Experiment if Iteration in MonitoredModel

--- server/app/routers/exceptions/iteration.py
+++ server/app/routers/exceptions/iteration.py
@@ -13,3 +13,17 @@ def iteration_assigned_to_monitored_model_exception():
         status_code=status.HTTP_400_BAD_REQUEST,
         detail="Iteration is assigned to monitored model. Cannot delete it. Please delete monitored model first."
     )
+
+
+def iteration_in_experiment_assigned_to_monitored_model_exception():
+    return HTTPException(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        detail="Iteration in experiment is assigned to monitored model. Cannot delete it. Please delete monitored model first."
+    )
+
+
+def iteration_in_experiment_in_project_assigned_to_monitored_model_exception():
+    return HTTPException(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        detail="Iteration in experiment in project is assigned to monitored model. Cannot delete it. Please delete monitored model first."
+    )
