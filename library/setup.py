047cb34a9357880ab78e3e518861d040edbe7578
directtt <72359171+directtt@users.noreply.github.com>
2024-01-08 00:25:10 +0100

    README logo fix

--- library/setup.py
+++ library/setup.py
@@ -9,7 +9,7 @@ README = (HERE/"README.md").read_text()
 
 setup(
    name="mlops-ai",
-   version="1.2.9",
+   version="1.3.0",
    description="Mlops-ai library for managing machine learning projects, experiments, iterations and datasets.",
    long_description=README,
    long_description_content_type="text/markdown",
